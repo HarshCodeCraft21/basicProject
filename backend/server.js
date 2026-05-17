3fa8f5c618839a8e279f3418b3d67440a4afe54e
HarshCodeCraft21 <harshj2103@gmail.com>
2026-05-17 13:54:55 +0530

    update server.js file

--- backend/server.js
+++ backend/server.js
@@ -1,23 +1,25 @@
 const app = require('./app');
 const connectDB = require('./config/db');
 
-
 require('dotenv').config();
 
 const PORT = process.env.PORT || 3000;
 
-
 const startServer = async () => {
-  
-  await connectDB();
+  try {
+    await connectDB();
+    console.log("MongoDB Connected Successfully");
 
-  
-  app.listen(PORT, () => {
-    console.log(`\n\x1b[35m==================================================\x1b[0m`);
-    console.log(`\x1b[1m\x1b[36m[Harsh Server]: Running in ${process.env.NODE_ENV || 'development'} mode\x1b[0m`);
-    console.log(`\x1b[1m\x1b[32m[Harsh Server]: Listening on: http://localhost:${PORT}\x1b[0m`);
-    console.log(`\x1b[35m==================================================\n\x1b[0m`);
-  });
+    app.listen(PORT, () => {
+      console.log(`\n==================================================`);
+      console.log(`[Harsh Server]: Running in ${process.env.NODE_ENV || 'development'} mode`);
+      console.log(`[Harsh Server]: Listening on PORT ${PORT}`);
+      console.log(`==================================================\n`);
+    });
+
+  } catch (error) {
+    console.log("Server Startup Error:", error.message);
+  }
 };
 
 startServer();
